48f225857f2b36d251df69ee7fb5580eeb718d07
diegodc03 <diegodecastro@usal.es>
2024-06-20 11:57:46 +0200

    finish

--- analizDePuertosParalelConBarraDeTiempo.py
+++ analizDePuertosParalelConBarraDeTiempo.py
@@ -22,7 +22,8 @@ def scan_ports(queue, sharedDict, ip, portMin, portMax, protocol):
             #dicPorts[port] = status
             sharedDict[port] = status
 
-        queue.put(1)
+        if(port % 2 == 0):
+            queue.put(1)
 
         
 
@@ -57,7 +58,8 @@ def scan_udp_port(ip, port):
 
 def printListOfPorts(dicPorts, protocol, ip, executionTime):
 
-    print(f"The execution time is: {executionTime}")
+    print(f"\nThe execution time is: {executionTime}\n")
+    print("The ports open and filtered are in the .txt file on the same folder of this program was executed")
 
     #for port in dicPorts:
     #    print(f"Port {port}/{protocol} on {ip} is {dicPorts[port]}")
@@ -76,18 +78,19 @@ def fileWithTheAccesiblePorts(ip, protocol, portMin, portMax, dicPorts, executio
 
 # q es el puntero a la cola
 def porcentajeUsed(q,portMax, portMin):
-    print("porcentajeCompleted")
+    print("porcentaje Completed")
 
     totalPorts = portMax - portMin + 1
 
     progressList = []
     total = portMin
 
+
     # Read results from the queue
     i=0
     while True:
         var = q.get()  
-        total = total + 1
+        total = total + 2
 
         portAnalyzing = total - portMin + 1
         portcent = portAnalyzing/totalPorts
@@ -97,9 +100,9 @@ def porcentajeUsed(q,portMax, portMin):
         # Rellena la lista de progreso con '#'
         progressList = ['#'] * num_hashes + ['-'] * (50 - num_hashes)
 
-        cadena = "".join(progressList)
+        stringProgress = "".join(progressList)
 
-        print(f"[{cadena}]{num_hashes*2}%", end='\r')
+        print(f"[{stringProgress}]{num_hashes*2}%", end='\r')
 
         if num_hashes == 50:
             break
@@ -117,76 +120,77 @@ def main():
     
     tupletype = ()
 
-    while(num_attempts <= max_attempts):
+    
 
-        if len(sys.argv) == 6:
-            ip=sys.argv[1] 
-            portMin=int(sys.argv[2])
-            portMax=int(sys.argv[3])
-            protocol = sys.argv[4]
-            num_process = int(sys.argv[5])
+    if len(sys.argv) == 6:
+        ip=sys.argv[1] 
+        portMin=int(sys.argv[2])
+        portMax=int(sys.argv[3])
+        protocol = sys.argv[4]
+        num_process = int(sys.argv[5])
 
-            protocol = protocol.upper()
+        protocol = protocol.upper()
 
 
-            if PORT_MIN < portMin < portMax < PORT_MAX and num_process > 2:
-                if protocol in {"TCP", "UDP"}:
+        if PORT_MIN < portMin < portMax < PORT_MAX and num_process >= 2:
+            if protocol in {"TCP", "UDP"}:
                     
-                    # Declaramos la cola, para asi poder comunicarnos entre procesos
-                    q = multiprocessing.Queue()
-                    # Declaramos la memoria compartida, en este caso un diccionario compartido
-                    manager = multiprocessing.Manager()
-                    sharedDict = manager.dict()
+                # Declaramos la cola, para asi poder comunicarnos entre procesos
+                q = multiprocessing.Queue()
+                # Declaramos la memoria compartida, en este caso un diccionario compartido
+                manager = multiprocessing.Manager()
+                sharedDict = manager.dict()
 
-                    # Primer proceso lo usamos para el porcentaje
-                    processPercent = multiprocessing.Process(target=porcentajeUsed, args=(q,portMin, portMax))
-                    processPercent.start()
+                # Primer proceso lo usamos para el porcentaje
+                processPercent = multiprocessing.Process(target=porcentajeUsed, args=(q, portMax, portMin))
+                processPercent.start()
                     
-                    processes = []
+                processes = []
 
-                    rangePorts = portMax - portMin + 1
-                    step = rangePorts // num_process
+                rangePorts = portMax - portMin + 1
+                step = rangePorts // num_process
 
-                    for i in range(num_process):
+                for i in range(num_process):
 
-                        startPort = portMin + i * step
-                        endPort = portMin + (i + 1) * step - 1
+                    startPort = portMin + i * step
+                    endPort = portMin + (i + 1) * step - 1
                         
-                        if i == num_process-1:
-                            endPort = portMax
+                    if i == num_process-1:
+                        endPort = portMax
                             
-                        args = (q, sharedDict, ip, startPort, endPort, protocol)
-                        p = multiprocessing.Process(target=scan_ports, args=args)
-                        p.start()
-                        processes.append(p)
+                    args = (q, sharedDict, ip, startPort, endPort, protocol)
+                    p = multiprocessing.Process(target=scan_ports, args=args)
+                    p.start()
+                    processes.append(p)
 
-                   
-                    # Esperamos a que todos los procesos terminen
-                    for proc in processes:
-                        proc.join()
-
-                    processPercent.join()
-
-                    #ordenamos el diccionario
-                    sharedDict_Ord = dict(sorted(sharedDict.items()))
-                    finalExecutionTime = time.time()
-                    executionTime = finalExecutionTime - executionStartTime
                     
-                    printListOfPorts(sharedDict_Ord, protocol, ip, executionTime)
-                    fileWithTheAccesiblePorts(ip,protocol, portMin, portMax, sharedDict_Ord, executionTime)
+                # Esperamos a que todos los procesos terminen
+                for proc in processes:
+                    proc.join()
 
-                    sys.exit()
+                processPercent.join()
+                   
+                #ordenamos el diccionario
+                sharedDict_Ord = dict(sorted(sharedDict.items()))
+                finalExecutionTime = time.time()
+                executionTime = finalExecutionTime - executionStartTime
+                    
+                printListOfPorts(sharedDict_Ord, protocol, ip, executionTime)
+                fileWithTheAccesiblePorts(ip,protocol, portMin, portMax, sharedDict_Ord, executionTime)
 
-                else:
-                    num_attempts += 1
-                    print("Put correct arguments\n")
-                    print("./nameOfProgramm <ipToAnalize> <PortMin> <PortMax> <Protocolo TCP/UDP> <numProcess >=2 >\n\n")
-            
+                sys.exit()
+
+            else:
+                print("Put correct arguments, only TCP or UDP must be on the fifth argument\n")
+                print("./nameOfProgramm <ipToAnalize> <PortMin> <PortMax> <Protocolo TCP/UDP> <numProcess >=2 >\n\n")
+
         else:
-            num_attempts += 1
-            print("Put correct arguments\n")
-            print("./nameOfProgramm <ipToAnalize> <PortMin> <PortMax> <Protocolo TCP/UDP> <numProcess>\n\n")
-
+            print("Put correct arguments, the portMin number mast be less than maxPort\n")
+            print("./nameOfProgramm <ipToAnalize> <PortMin> <PortMax> <Protocolo TCP/UDP> <numProcess >=2 >\n\n")
+            
+    else:
+        print("Put correct arguments, the arguments have to be 6\n")
+        print("./nameOfProgramm <ipToAnalize> <PortMin> <PortMax> <Protocolo TCP/UDP> <numProcess>\n\n")
     
 # Verificación si este archivo se ejecuta como el programa principal
 if __name__ == "__main__":
